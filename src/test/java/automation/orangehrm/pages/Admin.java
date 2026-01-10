67f027c35aed8e522423657732e999babac4cfc4
Negus Collis <neguscollis@Neguss-MacBook-Pro.local>
2026-01-10 11:57:12 -0600

    Adding Admin first row test

---
+++ src/test/java/automation/orangehrm/pages/Admin.java
@@ -0,0 +1,31 @@
+package automation.orangehrm.pages;
+
+import automation.orangehrm.utils.DriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Admin {
+
+    public void clickOnAdminLink(){
+        By adminLinkBy = By.xpath("(//a[@class='oxd-main-menu-item'])[1]");
+        WebElement adminLink = DriverManager.getDriver().findElement(adminLinkBy);
+
+        adminLink.click();
+    }
+
+    public List<String> getFirstRowText(){
+        By firstRowBy = By.xpath("(//div[@class='oxd-table-card'])[1]");
+        WebElement firstRow = DriverManager.getDriver().findElement(firstRowBy);
+
+        List<String> entries = Arrays.asList(firstRow.getText().split("\n"));
+
+        return entries;
+    }
+
+
+
+
+}
